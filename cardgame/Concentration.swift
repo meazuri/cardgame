e754962c595ecdf99639ba0739d507ee141609b7
seintsan <seintsan@seintsans-MacBook-Pro.local>
2020-04-22 23:33:22 +0800

    Simple Card Game Part 1

---
+++ cardgame/Concentration.swift
@@ -0,0 +1,13 @@
+//
+//  Concentration.swift
+//  cardgame
+//
+//  Created by seintsan on 22/4/20.
+//  Copyright © 2020 seintsan. All rights reserved.
+//
+
+import UIKit
+
+class Concentration: NSObject {
+
+}
