0eb09d7ba96987e0f944da964f1f24c3da238ac5
seintsan <seintsan@seintsans-MacBook-Pro.local>
2020-04-22 23:34:30 +0800

    Simple Card Game Part 1

---
+++ cardgame/ViewController.swift
@@ -0,0 +1,72 @@
+//
+//  ViewController.swift
+//  cardgame
+//
+//  Created by seintsan on 22/4/20.
+//  Copyright © 2020 seintsan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    lazy var game = Concentration(numberOfPairsOfCards: (cardButtons.count+1)/2)
+    var emojiesChoices = ["🦇","😱","🙀","😈","🎃","👻",
+                    "🍭","🍬","🍎"]
+    var flipCount = 0 {
+        didSet{
+
+            flipCountLabel.text = "Flips :\(flipCount)"
+        }
+    }
+    var emoji = [Int:String]()
+    func emoji (for card : Card) -> String {
+        
+        if emoji[card.identifier] == nil , emojiesChoices.count > 0 {
+            let randomIndex = Int( arc4random_uniform(UInt32(emojiesChoices.count)))
+            emoji[card.identifier] = emojiesChoices.remove(at: randomIndex)
+        }
+        
+        return emoji[card.identifier] ?? "?"
+    }
+    func updateViewFromModel() {
+        for index in cardButtons.indices {
+            let button = cardButtons[index]
+            let card = game.cards[index]
+            if card.isFaceUp  {
+                button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+                button.setTitle(emoji(for: card), for: UIControl.State.normal)
+
+            }else {
+                button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 0): #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+                button.setTitle("", for: UIControl.State.normal)
+            }
+            
+        }
+    }
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let card = cardButtons.firstIndex(of: sender){
+            game.chooseCard(at: card)
+            updateViewFromModel()
+        }else{
+            print("chosen card was not in carbuttons")
+        }
+    }
+   
+    
+//    func flipCard(withEmoji emoji: String , on Button :UIButton) {
+//        if Button.currentTitle == emoji {
+//            Button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+//            Button.setTitle("", for: UIControl.State.normal)
+//        }else{
+//            Button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+//            Button.setTitle(emoji, for: UIControl.State.normal)        }
+//    }
+    
+}
+
